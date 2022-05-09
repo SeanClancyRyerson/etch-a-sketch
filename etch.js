32acd5b03e4d855a320e36f0656d01ed70222480
Sean Ryerson <seanclancyryerson@gmail.com>
2022-05-08 20:39:28 -0400

    Begin work on hover event listener

--- etch.js
+++ etch.js
@@ -4,10 +4,12 @@ function createEtch(x){
     for (let i = 0; i < x; i++){
         for (let j = 0; j < x; j++){
             const block = document.createElement('div');
-            block.classList.add('etch-block');
+            block.classList.add('etch-block-blank');
             container.appendChild(block);
         }
     }
 }
+
+const blocks = document.querySelectorAll('')
 
 createEtch(16);
